4510c68d72b8668106bab84bf16b78bd3aa890ed
tarun <tarun.verma1n1@gmail.com>
2022-12-29 02:11:00 +0800

    test commit

---
+++ src/main/java/com/springBootDev/SpringBootDevApplication.java
@@ -0,0 +1,13 @@
+package com.springBootDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBootDev/SpringBootDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
