804ba3772219f232ed458157c052dc9e4fc43009
tarun <tarun.verma1n1@gmail.com>
2022-12-29 14:18:45 +0800

    adding request parameter

--- src/main/java/com/springBootDev/controller/TestController.java
+++ src/main/java/com/springBootDev/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.springBootDev.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/testController")
 public class TestController {
 	
 	@GetMapping("/getTestMessage")
