ac9733a707e7c2110330aa3ecfe2960c5b8b2048
tarun <tarun.verma1n1@gmail.com>
2022-12-29 22:40:27 +0800

    change request mapping

--- src/main/java/com/springBootDev/controller/TestController.java
+++ src/main/java/com/springBootDev/controller/TestController.java
@@ -1,14 +1,12 @@
 package com.springBootDev.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/testController")
 public class TestController {
 	
-	@GetMapping("/getTestMessage")
+	@RequestMapping("/getTestMessage")
 	public String getTestMessage() {
 		return "this is a test message";
 	}
