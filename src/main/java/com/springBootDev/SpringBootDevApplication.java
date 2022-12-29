b708764ffc0582a55dd630c4cec9566dfeedf8ca
tarun <tarun.verma1n1@gmail.com>
2022-12-29 23:06:31 +0800

    commit

--- src/main/java/com/springBootDev/SpringBootDevApplication.java
+++ src/main/java/com/springBootDev/SpringBootDevApplication.java
@@ -2,9 +2,17 @@ package com.springBootDev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDevApplication {
+	
+	@GetMapping("/getTestMessage")
+	public String getTestMessage() {
+		return "this is a test message";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDevApplication.class, args);
